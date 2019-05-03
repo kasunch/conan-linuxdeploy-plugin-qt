b8305a725a1b62d9f8c37b7c938e8fe5d00a405d
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-03 09:48:40 -0500

    Remove unrequired settings

--- build.py
+++ build.py
@@ -5,9 +5,5 @@ if __name__ == "__main__":
                ("https://api.bintray.com/conan/bincrafters/public-conan", "true", "bincrafters")]
     builder = ConanMultiPackager(remotes=remotes, build_policy="outdated")
     builder.add_common_builds()
-    # libstc++11 is required by gtest therefore by the whole build
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        settings["compiler.libcxx"] = 'libstdc++11'
-        settings["cppstd"] = '11'
 
     builder.run()
